ddf93f149008e23ae8a00c74cf912cbf244ec4ef
Andrei Tataranovich <andrei_tataranovich@epam.com>
2026-07-15 10:18:32 +0300

    chore: cleanup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -10,14 +10,7 @@ sudo apt-get install -y --no-install-recommends bats shellcheck
 sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 sudo chmod +x /usr/local/bin/yq
 
-curl -sLo /tmp/glab.deb https://gitlab.com/gitlab-org/cli/-/releases/v1.106.0/downloads/glab_1.106.0_linux_amd64.deb
-sudo apt-get install -y /tmp/glab.deb
-rm /tmp/glab.deb
-
 npm install -g semver
 
 # gh isn't logged in on first run (config lives in an empty named volume until `gh auth login`)
 gh auth setup-git -f --hostname github.com
-
-git config --global --unset-all credential.https://gitlab.deltixhub.com.helper || true
-git config --global --add credential.https://gitlab.deltixhub.com.helper '!glab auth git-credential'
